02cee7b11afe34d14baba589d0f9b638d9dd9e34
Tim Caswell <tim@creationix.com>
2015-08-07 11:37:48 -0500

    Show basic HTTP client using coro-net directly

--- src/main.lua
+++ src/main.lua
@@ -9,4 +9,18 @@ package.path = uv.cwd() .. "/lib/?/init.lua"
 -- Install global pretty-printer for easy debugging
 p = require('pretty-print').prettyPrint
 
-p(_G)
+local connect = require('coro-net').connect
+coroutine.wrap(function ()
+  local read, write, socket = connect({
+    host = "lit.luvit.io",
+    port = 80
+  })
+  p(read, write, socket)
+  write("GET / HTTP/1.0\r\nHost: lit.luvit.io\r\n\r\n")
+  for chunk in read do
+    p(chunk)
+  end
+  write()
+end)()
+
+uv.run()
