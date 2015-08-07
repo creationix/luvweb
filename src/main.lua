05a340c7b21482b813f68d65c863c09c6fa93f19
Tim Caswell <tim@creationix.com>
2015-08-07 11:08:51 -0500

    Add sample dependency library

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,12 @@
+-- Load the luv bindings from the cwd
+-- You must cd to this directory before starting this script.
 local uv = require('luv')
 
-print(uv)
+-- One line require system using submodules for dependencies.
+-- Change the lua search path to look for submodules in lib.
+package.path = uv.cwd() .. "/lib/?/init.lua"
+
+-- Install global pretty-printer for easy debugging
+p = require('pretty-print').prettyPrint
+
+p(_G)
