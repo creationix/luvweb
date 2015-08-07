b3395a37f39f835500cee2720826bf88205c5707
Tim Caswell <tim@creationix.com>
2015-08-07 16:15:12 -0500

    Use find_package for lua in bitop

--- deps/bitop.cmake
+++ deps/bitop.cmake
@@ -1,3 +1,8 @@
+find_package(Lua)
+if (LUA_FOUND)
+  include_directories(${LUA_INCLUDE_DIR})
+  link_directories(${LUA_LIBRARIES})
+endif (LUA_FOUND)
 if(APPLE)
   set(CMAKE_SHARED_MODULE_CREATE_C_FLAGS
     "${CMAKE_SHARED_MODULE_CREATE_C_FLAGS} -flat_namespace -undefined suppress"
